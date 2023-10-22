a975911bd1df7e2bf36b8358a2a0c0f56b4f2b16
n-kev1n <kevin_@tuta.io>
2023-10-23 00:05:09 +0200

    added component to hold trending topics

---
+++ src/pages/_components/trending.tsx
@@ -0,0 +1,15 @@
+import Container from "@/components/container";
+
+// type Props = {};
+
+const Trending = () => {
+  return (
+    <Container>
+      <h1>Trending on xedium</h1>
+      {/* TODO: Add Trending topics*/}
+      <div className="px-4 md:px-10 lg:px-16">Topic 01</div>
+    </Container>
+  );
+};
+
+export default Trending;
