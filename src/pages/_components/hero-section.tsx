64b3bef35291c21908f26a64f5617256d34e4f3b
n-kev1n <kevin_@tuta.io>
2023-10-23 00:03:28 +0200

    moved hero section code from App.tsx to a new file

---
+++ src/pages/_components/hero-section.tsx
@@ -0,0 +1,30 @@
+import Container from "@/components/container";
+import { Button } from "@/components/ui/button";
+
+const HeroSection = () => {
+  return (
+    <Container>
+      <div className="flex flex-col text-center gap-y-4 mt-4 ">
+        <div className="font-title text-5xl sm:text-7xl lg:text-8xl font-bold">
+          Stay Curius.
+        </div>
+        <div className="flex flex-col gap-y-8">
+          <p className="font-poppins text-lg">
+            Discover stories, thinking, and <br /> expertise from writers on any
+            topic.
+          </p>
+          <div>
+            <Button
+              size="lg"
+              className="rounded-full py-5 px-10 font-intel text-base"
+            >
+              Start reading
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HeroSection;
