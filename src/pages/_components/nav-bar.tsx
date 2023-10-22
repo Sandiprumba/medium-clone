3f5d6509cb4b74ad03c8771ed1d1034f905403a5
n-kev1n <kevin_@tuta.io>
2023-10-23 00:02:59 +0200

    added blur on navigation

--- src/pages/_components/nav-bar.tsx
+++ src/pages/_components/nav-bar.tsx
@@ -1,5 +1,8 @@
+// import Container from "@/components/container";
 import Container from "@/components/container";
 import { Button } from "@/components/ui/button";
+import { useScrollTop } from "@/hooks/use-scroll";
+import { cn } from "@/lib/utils";
 import { Link } from "react-router-dom";
 
 type TMenu = {
@@ -14,17 +17,28 @@ const Navbar = () => {
     { label: "Write", path: "/write" },
   ];
 
+  const scrolled = useScrollTop();
+
   return (
-    <div className="w-full z-50 bg-transparent backdrop-blur-sm fixed top-0">
+    <div
+      className={cn(
+        "w-full z-50 bg-amber-400/0 backdrop-blur-sm fixed top-0 transition-colors ease-in-out duration-200",
+        scrolled && "bg-white",
+      )}
+    >
       <div className="py-4 border-b-[1px] border-slate-950">
         <Container>
-          <div className="flex items-center justify-between">
+          <nav className="flex items-center justify-between">
             <div>
               <img src="/xedium.svg" alt="Logo" />
             </div>
-            <div className="flex items-center justify-between space-x-4 font-intel transition">
+            <div className="flex flex-1 items-center justify-end space-x-4 font-intel transition">
               {menus.map((menu) => (
-                <Link className="hidden lg:block px-1 text-sm" to={menu.path}>
+                <Link
+                  key={menu.label}
+                  className="hidden lg:block px-1 text-sm"
+                  to={menu.path}
+                >
                   {menu.label}
                 </Link>
               ))}
@@ -33,12 +47,15 @@ const Navbar = () => {
               </Link>
               <Button
                 size="sm"
-                className="rounded-full px-5 mx-4 bg-black text-sm"
+                className={cn(
+                  "rounded-full px-5 mx-4 bg-black text-sm",
+                  scrolled && "bg-green-700",
+                )}
               >
                 Get started
               </Button>
             </div>
-          </div>
+          </nav>
         </Container>
       </div>
     </div>
