9016c63f20414be4439da92f7b8cd44ec1864346
n-kev1n <kevin_@tuta.io>
2023-10-23 00:02:15 +0200

    refactored code into multiple components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import Container from "./components/container";
-import { Button } from "./components/ui/button";
-import Navbar from "./pages/_components/nav-bar";
+import HeroSection from "@_components/hero-section";
+import Navbar from "@_components/nav-bar";
+import Trending from "@_components/trending";
 
 function App() {
   return (
@@ -9,31 +9,13 @@ function App() {
       <div className="h-full w-full">
         <Navbar />
         <div className="h-[80dvh] flex items-center bg-amber-400">
-          <main className="h-full w-full flex items-center justify-start graphpaper">
-            <Container>
-              <div className="flex flex-col text-center gap-y-4 mt-4">
-                <div className="font-title text-5xl sm:text-7xl lg:text-8xl font-bold">
-                  Stay Curius.
-                </div>
-                <div className="flex flex-col gap-y-8">
-                  <p className="font-poppins text-lg">
-                    Discover stories, thinking, and <br /> expertise from
-                    writers on any topic.
-                  </p>
-                  <div>
-                    <Button
-                      size="lg"
-                      className="rounded-full py-5 px-10 font-intel text-base"
-                    >
-                      Start reading
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </Container>
+          <main className="h-full w-full flex items-center justify-start graphpaper border-b-black border-b-[1px]">
+            <HeroSection />
           </main>
         </div>
-        <div className="">Trending</div>
+        <div className="mt-8 ">
+          <Trending />
+        </div>
       </div>
     </>
   );
