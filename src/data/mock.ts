803a863701c158e1c4af070f147f60064a62967e
n-kev1n <kevin_@tuta.io>
2023-10-24 21:03:20 +0200

    added mock data to test Article UI

---
+++ src/data/mock.ts
@@ -0,0 +1,50 @@
+export const mockArticles = [
+  {
+    author: "Jane doe",
+    avatar:
+      "https://images.unsplash.com/photo-1624561272659-224ea122b2e9?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "State management in React Js",
+    published: "2023-01-09",
+    read_time: "2 min",
+  },
+  {
+    author: "John doe",
+    avatar:
+      "https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?auto=format&fit=crop&q=80&w=2080&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Getting started with Node Js",
+    published: "2023-09-19",
+    read_time: "2 min",
+  },
+  {
+    author: "Jane doe",
+    avatar:
+      "https://images.unsplash.com/photo-1624561272659-224ea122b2e9?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Effective Project management",
+    published: "2023-08-09",
+    read_time: "2 min",
+  },
+  {
+    author: "John doe",
+    avatar:
+      "https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?auto=format&fit=crop&q=80&w=2080&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Getting started with React Js",
+    published: "2023-10-09",
+    read_time: "2 min",
+  },
+  {
+    author: "Jane doe",
+    avatar:
+      "https://images.unsplash.com/photo-1624561272659-224ea122b2e9?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "State management in React Js",
+    published: "2023-01-09",
+    read_time: "2 min",
+  },
+  {
+    author: "Jane doe",
+    avatar:
+      "https://images.unsplash.com/photo-1624561272659-224ea122b2e9?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Effective Project management",
+    published: "2023-08-09",
+    read_time: "2 min",
+  },
+];
